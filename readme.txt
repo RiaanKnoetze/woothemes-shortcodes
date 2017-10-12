2cdb480dd941d8569c7e7c27c9253ca181411a2c
Riaan Knoetze <riaan@woothemes.com>
2017-10-12 16:49:08 +0200

    Initial commit.

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Woo Framework Shortcodes ===
+Contributors: Anand Shah
+Tags: shortcodes, woo-framework
+Requires at least: 3.9.0
+Tested up to: 4.4.1
+Stable tag: 1.0
+License: GPLv2 or later 
+
+== Description ==
+
+WooThemes uses WooFramework which comes with loads of shortcodes, if you have used the theme for a while you may have used the 
+shortcodes in your pages. Switching over to another theme becomes difficult as the shortcodes aren't supported in a non WooThemes theme. 
+With the help of this plugin easily switch to a non WooThemes theme but retain the framework shortcodes.
+
+Before installing this plugin you need to first deactivate the Woothemes theme and activate any other theme,
+and then install and activate this plugin.
+
+== Disclaimer ==
+
+Please note this is not an official WooThemes plugin. I have created this plugin
+with the hope that it will help someone switch to another theme like Storefront that doesn't
+have shortcodes that are provided by a WooThemes theme. All the copyrights and
+trademarks are the property of their respective owners.
+
+
+== Installation ==
+
+1. Upload to the plugins directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 1.0 =
+
+first version.
---
+++ woo-framework-shortcodes.php
@@ -0,0 +1,51 @@
+<?php
+if ( ! defined('ABSPATH') ) exit;
+/**
+* Plugin Name: WooThemes Shortcodes
+* Plugin URI: https://github.com/anandamd/woo-framework-shortcodes
+* Description: Easily switch to a non WooThemes theme but retain the framework shortcodes. Use shortcodes from WooFramework in another theme. 
+* Author: Riaan Knoetze
+* License: GPLv2
+* Version: 1.0.0
+* */
+
+
+// Add hooks for checking if Canvas is active
+register_activation_hook(__FILE__, 'plugin_activation_check');
+add_action ('init', 'plugin_activation_check');
+
+// Check if Canvas is active
+function plugin_activation_check() {
+	if ( get_template() != 'canvas' ) {
+
+		$functions_path = plugin_dir_path( __FILE__ ) . 'functions/';			
+		require_once ( $functions_path . 'admin-shortcodes.php' );	// Woo Shortcodes
+		
+		// Load certain files only in the WordPress admin.
+		if ( is_admin() ) {
+			require_once( $functions_path . 'admin-shortcode-generator.php' );
+		}
+
+		add_action( 'wp_head', 'wfs_wp_head' );	
+
+	} else {
+		//deactivate_plugins('woothemes-shortcodes/woo-framework-shortcodes.php');
+		add_action( 'admin_notices', 'plugin_activation_error_notice' );
+		//wp_die( 'Please deactivate Canvas.' );
+	}
+}
+
+// Display error notice if Canvas is active
+function plugin_activation_error_notice() {
+	?>
+		<div class="notice-error notice">
+		    <p><?php _e( 'Please switch to a different theme - this is required for the WooThemes Shortcodes plugin to work.', 'woothemes-shortcodes' ); ?></p>
+		</div>
+ 	<?php
+}
+
+
+function wfs_wp_head() {
+
+		woo_shortcode_stylesheet();
+} 
